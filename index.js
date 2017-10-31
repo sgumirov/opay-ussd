b9c8be0474bacf1565e774b53927f6c72141fee0
Shamil Gumirov <shamilg@opera.com>
2017-10-31 12:03:57 +0300

    added debugging nodejs script

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const { send } = require('micro');
+const parse = require('urlencoded-body-parser')
 const url = require('url');
+var http = require('http');
 
-module.exports = function (request, response) {
-  const { path } = url.parse(request.url);
+function parseCookies (request) {
+    var list = {},
+        rc = request.headers.cookie;
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        var parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
+
+module.exports = async function (req, res) {
+  console.log(req.headers);
+  console.log(parseCookies(req));
+  console.log(parseCookies(req).Session);
+  const { path } = url.parse(req.url);
   console.log(path);
+  const data = await parse(req);
+  console.log(data);
   if ("/1" === path)
-    send(response, 200, '1 Hello World!');
+    send(res, 200, '1 Hello World!');
   if ("/2" === path)
-    send(response, 200, '2 Hello World!');
+    send(res, 200, '2 Hello World!');
 };
