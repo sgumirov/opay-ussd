0f0716ea79659f9278a32666efaea564c405a637
Shamil Gumirov <shamil@gumirov.com>
2017-10-31 19:53:25 +0700

    added serve file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { send } = require('micro');
 const parse = require('urlencoded-body-parser')
 const url = require('url');
 var http = require('http');
+var fs = require('fs');
 
 function parseCookies (request) {
     var list = {},
@@ -15,6 +16,13 @@ function parseCookies (request) {
     return list;
 }
 
+function serveFile(res, filename) {
+  var mimeType = "text/xml";
+  res.writeHead(200, mimeType);
+  var fileStream = fs.createReadStream(filename);
+  fileStream.pipe(res);
+}
+
 module.exports = async function (req, res) {
   console.log(req.headers);
   console.log(parseCookies(req));
@@ -23,6 +31,11 @@ module.exports = async function (req, res) {
   console.log(path);
   const data = await parse(req);
   console.log(data);
+
+  if (path.includes("index.xml")) {
+    serveFile(req, "index.xml");
+  }
+
   if ("/1" === path)
     send(res, 200, '1 Hello World!');
   if ("/2" === path)
