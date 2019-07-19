a366f489cd540e3dc3431ca159307d5b0d563e43
Shamil Gumirov <shamil@gumirov.com>
2019-07-19 14:59:44 +0700

    Service 955 demo implemented

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const parse = require('urlencoded-body-parser')
 const url = require('url');
 var http = require('http');
 var fs = require('fs');
+const dir = "opay955";
 const session = require('micro-cookie-session')({
   name: 'session',
   keys: ['someverystringsecretstring1'],
   maxAge: 24 * 60 * 60 * 1000
 })
+var files = fs.readdirSync(dir);
 
-var files = ["index.xml", "airtime.xml", "fixed-amount.xml", "custom-amount.xml", "mpesa.xml", "payment.xml", "todo.xml"];
+console.log("Serving files:\n"+files)
+
 var last = {};
 
 function parseCookies (request) {
     var list = {},
         rc = request.headers.cookie;
-
     rc && rc.split(';').forEach(function( cookie ) {
         var parts = cookie.split('=');
         list[parts.shift().trim()] = decodeURI(parts.join('='));
@@ -28,7 +30,6 @@ function serveFile(res, abon, filename) {
   var mimeType = "text/html";
   res.writeHead(200, mimeType);
   var fileStream = fs.createReadStream(filename);
-//  send(res, 200, fileStream);
   fileStream.pipe(res);
   last[abon] = filename;
   console.log("last["+abon+"]="+filename);
@@ -45,15 +46,9 @@ module.exports = async function (req, res) {
     session(req, res);
     req.session.abon = abon
 
-    //user is back to finish payment
-    if (last[abon]==="src/mpesa.xml") {
-      serveFile(res, abon, "src/transaction-success.xml");
-      return;
-    }
-
     for (i = 0; i < files.length; i++) {
-      if (path.includes(files[i])) {
-        serveFile(res, abon, "src/"+files[i]);
+      if (path.includes("/"+files[i])) {
+        serveFile(res, abon, dir+"/"+files[i]);
         return;
       }
     }
